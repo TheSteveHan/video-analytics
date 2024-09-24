6ccc8679030840b06e27e2582bbae129101e69a4
Steve Han <hantianning@gmail.com>
2024-09-24 16:22:48 -0400

    set max size 4

--- selfie.js
+++ selfie.js
@@ -185,8 +185,8 @@ window.onChangeDevice = (e) => {
   navigator.mediaDevices.getUserMedia({
     audio: false,
     video: {
-      height: {"ideal": 4000, max: 4000},
-      width: {"ideal": 4000, max: 4000},
+      height: {"ideal": 3840, max: 3840},
+      width: {"ideal": 2160, max: 2160},
       facingMode:{"ideal": facing}
     }
   }).then((stream) => {
